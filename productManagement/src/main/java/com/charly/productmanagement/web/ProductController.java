41d31e075f902adb27b390a7f2f7576e606439ee
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-03-08 12:26:37 +0000

    Ajout du bouton Delete pour supprimer un produit et de la fonction delete dans le controller

--- productManagement/src/main/java/com/charly/productmanagement/web/ProductController.java
+++ productManagement/src/main/java/com/charly/productmanagement/web/ProductController.java
@@ -5,6 +5,7 @@ import com.charly.productmanagement.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,5 +23,11 @@ public class ProductController {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList",products);
         return "products";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name="id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
     }
 }
